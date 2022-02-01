97c3560817122c36ecfccae9e988ebc7862d65cb
Jieoz <47545984+Jieoz@users.noreply.github.com>
2022-02-01 15:57:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+cd package
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git
+cd lean
+rm -rf luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
